672bf0bdfa313a47bc2348d12138a0575610f92a
rofiah <akintundeadenike020606@gmail.com>
2025-10-06 23:15:35 +0100

    completed all day 05 exercises

--- day 05/countries.js
+++ day 05/countries.js
@@ -1,19 +1,39 @@
 // Exercise Level 2
 
 //1
-const countriesArray=['Albania','Bolivia','Canada','Denmark','Ethiopia','Finland','Germany', 'Hungary','Ireland','Japan','Kenya'
-]
-
+const countriesArray = [
+  "Albania",
+  "Bolivia",
+  "Canada",
+  "Denmark",
+  "Ethiopia",
+  "Finland",
+  "Germany",
+  "Hungary",
+  "Ireland",
+  "Japan",
+  "Kenya",
+];
 
 // 4
 
-const indexEthiopia=countriesArray.indexOf('Ethiopia')
+const indexEthiopia = countriesArray.indexOf("Ethiopia");
 
-if (indexEthiopia=== 4){
-    console.log('ETHIOPIA')
+if (indexEthiopia === 4) {
+  console.log("ETHIOPIA");
 } else {
-    console.log(countriesArray.unshift('Ethiopia'))
+  console.log(countriesArray.unshift("Ethiopia"));
 }
 
 // 5 is done in web_tech.js
 
+// Exercise Level 3
+
+console.log(countriesArray.slice(10));
+
+// 3
+const firstArray = countriesArray.slice(0, 6);
+const secondArray = countriesArray.splice(6);
+const unShifted = secondArray.unshift("Nigeria");
+console.log(firstArray);
+console.log(secondArray);
