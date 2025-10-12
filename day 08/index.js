7e33dfa5cfb44d0673fa3e33ab69cb772001d4ff
rofiah <akintundeadenike020606@gmail.com>
2025-10-12 01:01:08 +0100

    just started day 08 exercises

---
+++ day 08/index.js
@@ -0,0 +1,40 @@
+// OBJECTS
+//Exercise Level 1
+
+// 1
+const emptyDog = {};
+
+// 2
+console.log(emptyDog);
+
+// 3
+const dog = {
+  names: "Rob",
+  legs: 4,
+  color: "gold",
+  age: "Three months old",
+};
+
+// 4
+console.log(dog["names"]);
+console.log(dog.legs);
+console.log(dog.color);
+console.log(dog.age);
+
+// const person = {
+//   firstName: 'Asabeneh',
+//   age: 250,
+//   country: 'Finland',
+//   city:'Helsinki',
+//   skills: ['HTML', 'CSS', 'JS'],
+//   title: 'teacher',
+//   address: {
+//     street: 'Heitamienkatu 16',
+//     pobox: 2002,
+//     city: 'Helsinki'
+//   },
+//   getPersonInfo: function() {
+//     return `I am ${this.firstName} and I live in ${this.city}, ${this.country}. I am ${this.age}.`
+//   }
+// }
+// console.log(person)
