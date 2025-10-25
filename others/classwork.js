531ce1f813039c5fa3f33c74394d9399e87a028f
rofiah <akintundeadenike020606@gmail.com>
2025-10-25 23:48:05 +0100

    for learning outside the course

---
+++ others/classwork.js
@@ -0,0 +1,80 @@
+// const alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const statesInNaija =[
+  "Abia",
+  "Adamawa",
+  "Akwa Ibom",
+  "Anambra",
+  "Bauchi",
+  "Bayelsa",
+  "Benue",
+  "Borno",
+  "Cross River",
+  "Delta",
+  "Ebonyi",
+  "Edo",
+  "Ekiti",
+  "Enugu",
+  "FCT - Abuja",
+  "Gombe",
+  "Imo",
+  "Jigawa",
+  "Kaduna",
+  "Kano",
+  "Katsina",
+  "Kebbi",
+  "Kogi",
+  "Kwara",
+  "Lagos",
+  "Nasarawa",
+  "Niger",
+  "Ogun",
+  "Ondo",
+  "Osun",
+  "Oyo",
+  "Plateau",
+  "Rivers",
+  "Sokoto",
+  "Taraba",
+  "Yobe",
+  "Zamfara"
+]
+const numbers = "0123456789";
+let randomString1 = [];
+let randomNumbers = "";
+// console.log(statesInNaija.length)
+for (let i = 0; i < 4; i++) {
+  const randomStatePosition = Math.floor(Math.random() * statesInNaija.length);
+  const randStateThreeLtr = statesInNaija[randomStatePosition].toUpperCase().slice(0,3);
+  console.log(randStateThreeLtr)
+
+//   const randomNumPosition = Math.floor(Math.random() * numbers.length);
+//   const randNumber = numbers[randomNumPosition];
+// console.log(randNumber)
+//   randomNumbers += randNumber;
+     
+//   if (randomString1.length < 3) {
+//       randomString1 += randStateThreeLtr;
+// }
+}
+// console.log(randomString1)    
+// console.log(randomNumbers)
+// console.log (`${randomString1}-${randomNumbers}`)
+
+
+
+
+
+
+
+
+
+
+
+// for (let i = 0; i < 4; i++){
+//     const randomPosition = Math.floor(Math.random() * numbers.length)
+//     const randnumber = numbers[randomPosition]
+//     console.log(randnumber)
+//     randomString2 += randnumber
+
+// }
+// console.log(`${randomString1}-${randomString2}`)
