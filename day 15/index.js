e850d3eb6168d044e14d6edb91ce6a5b30355bcc
rofiah <akintundeadenike020606@gmail.com>
2025-12-03 12:33:38 +0100

    day 15

--- day 15/index.js
+++ day 15/index.js
@@ -48,36 +48,11 @@ console.log(cat.sayHello())
 
 // Exercise Level 2
 // 1
+class Cat extends AnimalClass{
+constructor(name, age, color, legs){
+super  (name, age, color, legs)
+}
+}
+console.log(Cat)
 
 
-
-// class Person {
-//   constructor(firstName, lastName, age, country, city) {
-//     this.firstName = firstName
-//     this.lastName = lastName
-//     this.age = age
-//     this.country = country
-//     this.city = city
-//   }
-//   getFullName() {
-//     const fullName = this.firstName + ' ' + this.lastName
-//     return fullName
-//   }
-// }
-
-// const person1 = new Person('Asabeneh', 'Yetayeh', 250, 'Finland', 'Helsinki')
-// const person2 = new Person('Lidiya', 'Tekle', 28, 'Finland', 'Espoo')
-
-// console.log(person1.getFullName())
-// console.log(person2.getFullName())
-
-// class Student extends Person {
-//   saySomething() {
-//     return 'I am a child of the person class'
-//   }
-// }
-// const s1 = new Student('Asabeneh', 'Yetayeh', 'Finland', 250, 'Helsinki')
-// console.log(s1)
-// console.log(s1.saySomething())
-// console.log(s1.getFullName())
-// // console.log(s1.getPersonInfo())
