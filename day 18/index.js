298ca9277994e1fcfb23443d54eceb25a6fa9eaf
rofiah <akintundeadenike020606@gmail.com>
2025-12-27 10:14:08 +0100

    ise

--- day 18/index.js
+++ day 18/index.js
@@ -1,64 +1,48 @@
-//Callback
-const doSomething = callback => {
-  setTimeout(() => {
-    const skills = ['HTML', 'CSS', 'JS']
-    callback(false, skills)
-  }, 20)
-}
+// PROMISES
+// Exercise Level 1
+// 1
 
-const callback = (err, result) => {
-  if (err) {
-    return console.log(err)
-  }
-  return console.log(result)
-}
+const countriesAPI = 'https://restcountries.com/v3.1/all?fields=name,capital,languages,population,area'
+fetch(countriesAPI)
+.then(response => response.json())
+.then(data =>{
+    data.forEach(country => {
+        console.log(
+            `Country:${country.name.common}`, 
+            `Capital:${country.capital }`, 
+            `Languages:${country.languages}`,
+            `Areas:${country.area}`
+        )
+    });
+})
+.catch(error=>console.error(error))
 
-doSomething(callback)
-
-const promise = new Promise((resolve, reject) => {
-setTimeout(()=>{
-    const skills = ['HTML', 'CSS', 'JS']
-if (skills.length > 0){
-    resolve (skills)
-}else{
-    reject('Something is wrong')
-}
-}, 2000)
+// Exercise level 2
+// 1
+const catsAPI = 'https://api.thecatapi.com/v1/breeds'
+fetch(catsAPI)
+.then(response=>response.json())
+.then(cats => {
+    catNames=cats.map(catN=>catN.name)
+    console.log(catNames)
+    console.log(cats)
+    const totalAverage=cats.reduce((sum, acc)=>{
+   
+    })
 })
 
-promise
-.then(result=>{
-    console.log(result)
-})
-.catch(error => console.log(error))
+//Exercise level 3
+// 1
 
 
 
-// const url = 'https://restcountries.com/v2/all' // countries api
-// fetch(url)
-//   .then(response => response.json()) // accessing the API data as JSON
-//   .then(data => {
-//     // getting the data
-//     console.log(data)
-//   })
-//   .catch(error => console.error(error)) // handling error if something wrong happens
-
-  const square = async function (n) {
-  return n * n
-}
-
-console.log(square(2))
 
 
-const fetchData = async () => {
-  try {
-    const response = await fetch(url)
-    const countries = await response.json()
-    console.log(countries)
-  } catch (err) {
-    console.error(err)
-  }
-}
-console.log(fetchData())
+
+
+
+
+
+
 
 
